3ab5e7e18e1a16eb8af12fb7be0ec1c777812d22
AbdullahRai <raiabdullah0090@gmail.com>
2021-07-13 03:14:56 +0500

    Database connection

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+// connecting database 
 
+connectDB();
 app.get('/', (req, res) => res.send('API is running smothly'));
 
 const PORT = process.env.PORT || 5000;
