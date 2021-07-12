3432f5864177cefabb23bf03f4841b85de7f7766
AbdullahRai <raiabdullah0090@gmail.com>
2021-07-13 02:15:53 +0500

    response changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API is running'));
+app.get('/', (req, res) => res.send('API is running smoothly'));
 
 const PORT = process.env.PORT || 5000;
 
