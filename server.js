ce466e53cda594f17446a0f5a50dfe6d49ca94e2
AbdullahRai <raiabdullah0090@gmail.com>
2021-07-14 14:52:55 +0500

    user registration and validation

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 // connecting database 
 
 connectDB();
+// init Middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => res.send('API is running smothly'));
 
 // Defining Routes
